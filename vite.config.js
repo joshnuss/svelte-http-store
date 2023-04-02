34c94284403efe234f94ae1b9fabe14a4377c494
Josh Nussbaum <joshnuss@gmail.com>
2023-04-01 21:33:35 -0400

    Run tests on a single thread

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [sveltekit()],
   test: {
+    singleThread: true,
     include: ['src/**/*.{test,spec}.{js,ts}']
   }
 })
